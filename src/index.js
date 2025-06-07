a10f290b11855e70f011e6002f228ac803c2092b
Alejandro <snackbarchat@gmail.com>
2025-06-07 02:02:32 -0300

    Setup env vars, CORS and shared axios

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 import billRoutes from './routes/billRoutes.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/errorHandler.js';
 import './reminder.js';
 
+dotenv.config();
+
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(logger);
 
 app.use('/bills', billRoutes);
